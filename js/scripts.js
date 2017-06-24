9c83a5afb439c901c6088d9eda0c4400635ad37d
Sara Spink <spinkbot@gmail.com>
2017-06-24 11:05:30 -0600

    add user logic and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,51 @@
-
-var PingPongResult = function(result) {
-  return userNumber;
+var PingPong = function(pingPongInput) {
+  if ((pingPongInput % 3 === 0) && (pingPongInput % 5 === 0)) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 
 $(document).ready(function() {
   $("form#ping-pong").submit(function(event) {
     event.preventDefault();
-    var userNumber = parseInt($("input#inputNumber").val());
-    var result = PingPongResult(inputNumber);
-    $("#result").text(result);
+    var pingPongInput = parseInt($("input#pingPongNum").val());
+    var result = PingPong(pingPongInput);
 
+    // $(".myNum").text(pingPongInput);
+
+    // if (!result) {
+    //   $(".not").text("not");
+    // } else {
+    //   $(".not").text("");
+    // }
+
+    $("#result").show();
   });
 });
+
+
+
+
+
+
+// var userNum = function(userNum) {
+//   if (userNum % 3 === 0) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+//
+// $(document).ready(function() {
+//   $("form#ping-pong").submit(function(event) {
+//     event.preventDefault();
+//     var userNumber = parseInt($("input#inputNumber").val());
+//     var result = PingPongResult(inputNumber);
+//
+//     $(".myNum").text(userNumber);
+//     $("#result").show();
+//
+//   });
+// });

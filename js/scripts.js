636f241000e5b24e7a0d4438385b82188df7a765
Sara Spink <spinkbot@gmail.com>
2017-06-24 13:28:54 -0600

    create a variable that is an empty array

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,13 @@
 var PingPong = function(pingPongInput) {
-  // if ((pingPongInput % 3 === 0) && (pingPongInput % 5 === 0)) {
-  //   return true;
-  // } else {
-  //   return false;
-  // }
+  if (pingPongInput % 15 === 0) {
+    $(".myNum").text("PingPong!");
+  } else if ((pingPongInput % 3 === 0) && (pingPongInput % 5 !== 0)) {
+    $(".myNum").text("Ping");
+  } else if ((pingPongInput % 5 === 0) && (pingPongInput % 3 !== 0)) {
+    $(".myNum").text("Pong");
+  } else {
+    $(".myNum").text(pingPongInput);
+  }
 };
 
 
@@ -12,50 +16,8 @@ $(document).ready(function() {
     event.preventDefault();
     var pingPongInput = parseInt($("input#pingPongNum").val());
     var result = PingPong(pingPongInput);
-
-    // $(".myNum").text(pingPongInput);
-
-    if (pingPongInput % 15 === 0) {
-      $(".myNum").text("PingPong!");
-    } else if ((pingPongInput % 3 === 0) && (pingPongInput % 5 !== 0)) {
-      $(".myNum").text("Ping");
-    } else if ((pingPongInput % 5 === 0) && (pingPongInput % 3 !== 0)) {
-      $(".myNum").text("Pong");
-    } else {
-      $(".myNum").text(pingPongInput);
-    }
-
-
-      // $(".not").text("not");
-    // } else {
-    //   $(".not").text("");
-    // }
+    var pingPongArr = []
 
     $("#result").show();
   });
 });
-
-
-
-
-
-
-// var userNum = function(userNum) {
-//   if (userNum % 3 === 0) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// };
-//
-// $(document).ready(function() {
-//   $("form#ping-pong").submit(function(event) {
-//     event.preventDefault();
-//     var userNumber = parseInt($("input#inputNumber").val());
-//     var result = PingPongResult(inputNumber);
-//
-//     $(".myNum").text(userNumber);
-//     $("#result").show();
-//
-//   });
-// });

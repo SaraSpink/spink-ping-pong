40625419a77b769222010df1276bca0f4529f6a0
Sara Spink <spinkbot@gmail.com>
2017-06-24 12:11:43 -0600

    be able to return strings according to the parameters but the results are not appended yet

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
 var PingPong = function(pingPongInput) {
-  if ((pingPongInput % 3 === 0) && (pingPongInput % 5 === 0)) {
-    return true;
-  } else {
-    return false;
-  }
+  // if ((pingPongInput % 3 === 0) && (pingPongInput % 5 === 0)) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
 };
 
 
@@ -13,12 +13,16 @@ $(document).ready(function() {
     var pingPongInput = parseInt($("input#pingPongNum").val());
     var result = PingPong(pingPongInput);
 
-    $(".myNum").text(pingPongInput);
+    // $(".myNum").text(pingPongInput);
 
-    if (!result) {
-      $(".myNum").text(pingPongInput);
+    if (pingPongInput % 15 === 0) {
+      $(".myNum").text("PingPong!");
+    } else if ((pingPongInput % 3 === 0) && (pingPongInput % 5 !== 0)) {
+      $(".myNum").text("Ping");
+    } else if ((pingPongInput % 5 === 0) && (pingPongInput % 3 !== 0)) {
+      $(".myNum").text("Pong");
     } else {
-      $(".myNum").text("PingPong!")
+      $(".myNum").text(pingPongInput);
     }
 
 
